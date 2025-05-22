8caead99337b8f0ca301535ef680499721aba266
Jacob Dev <deverejt@miamioh.edu>
2025-05-21 20:13:58 -0600

    Update site

--- src/Contact.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contact() {
-  return (
-    <div className="text-center mt-8 space-y-6">
-      <div className="text-2xl font-bold font-merriweather">Contact</div>
-    </div>
-  );
-}
--- src/People.tsx
+++
@@ -1,3 +0,0 @@
-export default function People() {
-  return <div className="text-center text-2xl font-bold mt-8">People</div>;
-}
